73bbe204530cdef517f706a0a8051897340cfb62
isaksengeir <geir.isaksen@uit.no>
2021-10-12 10:48:58 +0200

    Init of appNRIS.py

---
+++ appNRIS.py
@@ -0,0 +1,22 @@
+from PyQt5 import QtWidgets
+from UI.MainWindow import Ui_MainWindow
+import sys
+
+
+class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+        self.setupUi(self)
+        self.setWindowTitle("appNRIS")
+
+        self.show()
+
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    window = MainWindow()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
